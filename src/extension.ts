5eb9768acd6b5b55eed5099c371be890c911f8d1
Ram <vaishnav.rd@gmail.com>
2020-07-09 20:43:11 +0530

    new version 1.4.0

--- src/extension.ts
+++ src/extension.ts
@@ -1,45 +1,61 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 let myStatusBarItem: vscode.StatusBarItem;
 
 export function activate({ subscriptions }: vscode.ExtensionContext) {
+  // register a command that is invoked when the status bar
+  // item is selected
+  const myCommandId = "ext.showSelectionCount";
+  subscriptions.push(
+    vscode.commands.registerCommand(myCommandId, () => {
+      let n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
+      vscode.window.showInformationMessage(
+        `${n} line${n ? "s" : ""} selected.`
+      );
+    })
+  );
 
-	// register a command that is invoked when the status bar
-	// item is selected
-	const myCommandId = 'ext.showSelectionCount';
-	subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
-		let n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
-		vscode.window.showInformationMessage(`${n} line${n ? 's' : ''} selected.`);
-	}));
+  // create a new status bar item that we can now manage
+  myStatusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    10000
+  );
+  myStatusBarItem.command = myCommandId;
+  subscriptions.push(myStatusBarItem);
 
-	// create a new status bar item that we can now manage
-	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-	myStatusBarItem.command = myCommandId;
-	subscriptions.push(myStatusBarItem);
+  // register some listener that make sure the status bar
+  // item always up-to-date
+  subscriptions.push(
+    vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem)
+  );
+  subscriptions.push(
+    vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem)
+  );
 
-	// register some listener that make sure the status bar 
-	// item always up-to-date
-	subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
-	subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
-
-	// update status bar item once at start
-	updateStatusBarItem();
+  // update status bar item once at start
+  updateStatusBarItem();
 }
 
 function updateStatusBarItem(): void {
-	let n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
-	if (n > 1) {
-		myStatusBarItem.text = `$(megaphone) ${n} lines`;
-		myStatusBarItem.show();
-	} else {
-		myStatusBarItem.hide();
-	}
+  let n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
+  if (n > 1) {
+    myStatusBarItem.text = `$(list-selection) ${n} lines`;
+    myStatusBarItem.tooltip = `${n} lines selected`;
+    myStatusBarItem.show();
+  } else {
+    myStatusBarItem.hide();
+  }
 }
 
-function getNumberOfSelectedLines(editor: vscode.TextEditor | undefined): number {
-	let lines = 0;
-	if (editor) {
-		lines = editor.selections.reduce((prev, curr) => prev + (curr.end.line - curr.start.line), 1);
-	}
-	return lines;
+function getNumberOfSelectedLines(
+  editor: vscode.TextEditor | undefined
+): number {
+  let lines = 0;
+  if (editor) {
+    lines = editor.selections.reduce(
+      (prev, curr) => prev + (curr.end.line - curr.start.line),
+      1
+    );
+  }
+  return lines;
 }
